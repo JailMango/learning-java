ace6a6d3c038560af9998e0ac29e627ddabfc504
he.gang33 <he.gang33@iwhalecloud.com>
2020-09-11 13:19:09 +0800

    #spring5核心原理

---
+++ java-spring/src/main/java/com/jailmango/spring/book/principles/chapter01/Case1_1_1.java
@@ -0,0 +1,33 @@
+package com.jailmango.spring.book.principles.chapter01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Case1_1_1
+ *
+ * @author he.gang33
+ * @CreateDate 2020/9/11
+ * @see com.jailmango.spring.book.principles.chapter01
+ * @since R9.0
+ */
+public class Case1_1_1 {
+    
+    /**
+     * logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Case1_1_1.class);
+
+    /**
+     * main
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+
+    }
+
+    private static interface ICourse {
+        
+    }
+}
