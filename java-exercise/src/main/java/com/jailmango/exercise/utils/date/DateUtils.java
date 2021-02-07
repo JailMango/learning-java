fd6d51e66c8830e6b1e3da3edd28cee3069d218e
he.gang33 <he.gang33@iwhalecloud.com>
2021-02-07 13:56:52 +0800

    #exercise

---
+++ java-exercise/src/main/java/com/jailmango/exercise/utils/date/DateUtils.java
@@ -0,0 +1,29 @@
+package com.jailmango.exercise.utils.date;
+
+import java.util.Date;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * DateUtils
+ *
+ * @author he.gang33
+ * @CreateDate 2021/2/1
+ * @see com.jailmango.exercise.utils.date
+ * @since R9.0
+ */
+@Slf4j
+public class DateUtils {
+
+    /**
+     * main
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        Date now = new Date(1612170180000L);
+        long value = now.getTime();
+
+        log.info("end...");
+    }
+}
