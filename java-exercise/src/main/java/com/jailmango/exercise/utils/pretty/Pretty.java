2e96fdab8b8a5c9a4c3ff4ce02e67dc2e030b73c
he.gang33 <he.gang33@iwhalecloud.com>
2021-03-08 14:49:53 +0800

    #exercise

---
+++ java-exercise/src/main/java/com/jailmango/exercise/utils/pretty/Pretty.java
@@ -0,0 +1,76 @@
+package com.jailmango.exercise.utils.pretty;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Pretty
+ *
+ * @author he.gang33
+ * @CreateDate 2021/2/18
+ * @see com.jailmango.exercise.utils.pretty
+ * @since R9.0
+ */
+@Slf4j
+public class Pretty {
+
+    /**
+     * main
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) throws InterruptedException {
+        MyThread1 t = new MyThread1("my-thread");
+        t.start();
+
+        Thread.sleep(10000);
+
+        t.interrupt();
+
+        while (true) {
+
+        }
+    }
+
+    private static class MyThread1 extends Thread {
+
+        public volatile boolean exit = false;
+
+        public MyThread1(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            log.info("my-thread doing...");
+
+            Thread thread = new Thread(() -> {
+                while (true) {
+                    log.info("daemon...");
+                    try {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.setName("thread-1");
+            thread.setDaemon(true);
+            thread.start();
+
+            while (!exit) {
+                log.info("my-thread doing...");
+                try {
+                    Thread.sleep(500);
+                }
+                catch (InterruptedException e) {
+                    exit = true;
+                    e.printStackTrace();
+                }
+            }
+
+            thread.interrupt();
+        }
+    }
+}
---
+++ java-exercise/src/main/java/com/jailmango/exercise/utils/test/LoopCase.java
@@ -0,0 +1,47 @@
+package com.jailmango.exercise.utils.test;
+
+/**
+ * LoopCase
+ *
+ * @author he.gang33
+ * @CreateDate 2021/3/4
+ * @see com.jailmango.exercise.utils.test
+ * @since R9.0
+ */
+public class LoopCase {
+
+    /**
+     * main
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        boolean exit = false;
+        int size = 331;
+        int[] datasource = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            datasource[i] = i;
+        }
+
+        int delta = (size - 2) / 8;
+
+
+        for (int m = 0; m < size;) {
+            System.out.println(m);
+
+            if (exit) {
+                break;
+            }
+
+            m += delta;
+
+            if (m >= size) {
+                m = size - 1;
+                exit = true;
+            }
+        }
+
+        System.out.println("exit...");
+    }
+}
