1550aba3ea873bbd133c7fe962f2fa3e636a3671
jailmango <eason.hegang@gmail.com>
2021-12-02 11:30:57 +0800

    ThreadPool

---
+++ java-exercise/src/main/java/com/jailmango/exercise/utils/if_else/SwitchCase.java
@@ -0,0 +1,29 @@
+package com.jailmango.exercise.utils.if_else;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * SwitchCase
+ *
+ * @author he.gang33
+ * @CreateDate 2021/11/30
+ * @see com.jailmango.exercise.utils.if_else
+ */
+@Slf4j
+public class SwitchCase {
+
+    /**
+     * main
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        String str = null;
+        switch (str) {
+            case "1":
+                log.info("1");
+            default:
+                log.info("null");
+        }
+    }
+}
